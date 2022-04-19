07de658bf6e8fa4e65d86658a5257bc3987f6773
Saket <sakettiwari076@gmail.com>
2022-04-20 04:06:53 +0530

    CSS addeed

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import '../public/theim.png'
 export default function Home() {
   return (
-    <div>
-    <nav className="bg-black py-5 border-gray-200 px-2 sm:px-4 text-2xl  rounded dark:bg-gray-800">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com" class="flex items-center">
-          <img src="/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
-        <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-          <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-        </button>
-        <div className=" hidden w-full md:block md:w-auto" id="mobile-menu">
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  <div>
 
-    <div className='container px-2 pt-2 text-white w-3/4'>
-        <div className='flex justify-center py-52'>
-          <p className='text-4xl tracking-wide	'>HI THERE , I 'M <h1 className='text-8xl font-bold
-          '>saket tiwari</h1> </p> 
+    <div className='container px-2 pt-2 text-white '>
+    
+        <div className='flex justify-center pt-64 '>
+          <p className='text-4xl tracking-wide font-mono 	'>HI THERE , I 'M <h1 className='text-9xl font-bold 
+            '>saket tiwari.</h1> <p className='text-3xl pt-7 tracking-wide text-green-500 font-mono '>WEB DEVELOPER  { " + "}OPEN SOURCE ENTHUSIAST </p> </p> 
+              <div className='w-72 h-40'>
+            <img src='https://assets.website-files.com/5e51c674258ffe10d286d30a/5e535720d39923630d5487d7_peep-47.svg  ' className='img1'></img>
+            </div>
 
         </div>
-        <div className=' '>
-          <span class="inline">display: inline</span>
+       <div className='flex justify-center text-2xl font-mono pt-10 mr-36 '>
+       <p className=''>Developer with working experience in REACT JS ,<br></br> NEXT JS , VCS-GIT & GITHUB ,<br></br>  BOOTSTRAP 5 , TAILWIND CSS and HTML CSS and JAVASCRIPT <br></br> AUTH0 , GOOGLE ANALYTICS and now working on MERN STACK ..<br></br>
+       </p>
+       </div>
+       <div className='flex justify-center margin2 py-10 '>
+       <button class="h-10 rounded-full  px-20 m-2 pt text-black text-xl font-mono font-extrabold  transition-colors duration-150 bg-green-500  focus:shadow-outline hover:bg-green-800">Contact me</button>
 
-          While using the property <span class="inline-block">display: inline-block</span>
-          will wrap the element to prevent the text inside from extending beyond its parent.
-
-          Lastly, using the property <span class="block">display: block</span>
-          will put the element on its own line and fill its parent.
-        </div>
+</div>
+       
       </div>
       </div>
   )
