24482f232d0bf9815353e81226e95adcb59a0d91
Saket <sakettiwari076@gmail.com>
2022-04-20 01:33:41 +0530

    Basic design added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import '../styles/globals.css' ;
+import react  from 'react';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+ <div className=' '> 
+    
+     
+   <Component {...pageProps} />
+   
+   
+   </div>
+  
+
+
+);
+  };
 
 export default MyApp
