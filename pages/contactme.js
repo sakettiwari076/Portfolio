9957b632256a7feff912ec9236950a5aa5567cf9
Saket <sakettiwari076@gmail.com>
2022-04-20 19:35:45 +0530

    Contact me added and React icons

--- pages/contactme.js
+++ pages/contactme.js
@@ -3,15 +3,26 @@ import React from 'react'
 const contactme = () => {
   return (
    <div className='flex justify-center text-white pt-20 '>
-   <div className='text-7xl font-mono font-bold'>
-   <h1>REACH ME VIA</h1>
+   <div className='text-7xl font-mono  font-bold'>
+   <h1 className=''> REACH ME VIA</h1>
    </div>
-   <div className='icons flex '>
-   <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdDjv79776rzru8iFnl1qT7uxjv-4suQBjkfUSzSEQzZYoLE6yB3eRBKwGH6qrYyVuNUE&usqp=CAU' className=''/>
-   <img src='' className=''/>
-   <img src='' className=''/>
-   <img src='' className=''/>
-   <img src='' className=''/>
+   <div className='icons flex font-mono text-4xl  space-x-14 pt-72'>
+<ul className='space-y-12 positionli'>
+<a className='' href=''>
+<li>GITHUB</li></a>
+<a href=''>
+<li>GITHUB</li></a>
+<a href=''>
+<li>GITHUB</li></a>
+<a href=''>
+<li>GITHUB</li></a>
+<a href=''>
+<li>GITHUB</li></a>
+<a href=''>
+<li>GITHUB</li></a>
+
+</ul>
+
 
  
 
--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import '../public/theim.png'
+import Link from 'next/link'
+import {VscGithubInverted} from 'react-icons/Vsc'
+import {FaInstagramSquare} from 'react-icons/fa'
+
+import { icons } from 'react-icons'
 
 export default function Home() {
   return (
 
   <div>
 
-
   
     <div className='container px-2 pt-2 text-white '>
     
         <div className='flex justify-center pt-64 ml-72 '>
-          <p className='text-4xl tracking-wide font-mono  	'>HI THERE , I 'M <h1 className='text-9xl font-bold 
+          <p className='text-4xl tracking-wide font-mono  	'>HI THERE , I 'M  <h1 className='text-9xl font-bold line-1 anim-typewriter
             '>saket tiwari.</h1> <p className='text-3xl pt-7 tracking-wide text-green-500 font-mono '>WEB DEVELOPER  { " + "}OPEN SOURCE ENTHUSIAST </p> </p> 
               <div className='w-72 h-40'>
             <img src='https://assets.website-files.com/5e51c674258ffe10d286d30a/5e535720d39923630d5487d7_peep-47.svg  ' className='img1'></img>
@@ -25,10 +29,16 @@ export default function Home() {
        </p>
        </div>
        <div className='flex justify-center margin2 py-10 '>
-       <button className="h-10 rounded-full  px-20 m-2 pt text-black text-xl font-mono font-extrabold  transition-colors duration-150 bg-green-500  focus:shadow-outline hover:bg-green-800">Contact me</button>
+
+      <a href='/contactme'><button className="h-10 rounded-full  px-20 m-2 pt text-black text-xl font-mono font-extrabold  transition-colors duration-150 bg-green-500  focus:shadow-outline hover:bg-green-800">Contact me</button></a>
 
 </div>
        
+      </div>
+      <div className=''>
+    <VscGithubInverted className='icon'/>
+    <FaInstagramSquare/>
+      
       </div>
       </div>
   )
